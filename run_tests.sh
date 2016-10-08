d832631a8e075526431d41ae628d4577c1030e81
Pedro Matias <pedro@pmatias.com>
2016-10-07 17:32:17 -0700

    Modify codebase structure

--- run_tests.sh
+++ run_tests.sh
@@ -2,13 +2,16 @@
 
 
 for i in {1..12}; do
-    ./rbftest$i 2> /dev/null
+    rbf/rbftest$i 2> /dev/null
     if [ $? -ne 0 ]; then
         exit $?
     fi
 
     if [[ $i -eq 8 ]]; then
-        ./rbftest${i}b 2> /dev/null
+        rbf/rbftest${i}b 2> /dev/null
+        if [ $? -ne 0 ]; then
+            exit $?
+        fi
     fi
 
 done
