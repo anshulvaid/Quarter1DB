f05f0d47cdae87f9e5a9331292c02743998fbecc
Pedro Matias <pedro@pmatias.com>
2016-10-21 17:50:40 -0700

    Updated run_tests script
    So it doesnt run private tests automatically

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+RUN_PRIVATE=0
+if [[ $# -ge 1 ]]; then
+    RUN_PRIVATE=1
+fi
 
 for i in {1..12}; do
     rbf/rbftest$i || exit $?
@@ -12,11 +16,13 @@ done
 
 
 # Run private tests
-rbf/rbftest_p0 || exit $?
-rbf/rbftest_p1 || exit $?
-rbf/rbftest_p1b || exit $?
-rbf/rbftest_p1c || exit $?
-rbf/rbftest_p2 || exit $?
-rbf/rbftest_p2b || exit $?
-rbf/rbftest_p3 || exit $?
-rbf/rbftest_p4 || exit $?
+if [[ RUN_PRIVATE -eq 1 ]]; then
+    rbf/rbftest_p0 || exit $?
+    rbf/rbftest_p1 || exit $?
+    rbf/rbftest_p1b || exit $?
+    rbf/rbftest_p1c || exit $?
+    rbf/rbftest_p2 || exit $?
+    rbf/rbftest_p2b || exit $?
+    rbf/rbftest_p3 || exit $?
+    rbf/rbftest_p4 || exit $?
+fi
